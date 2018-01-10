5251dc3309406259391beec67cd0f184a418cc5f
Carlos Vicente <carlos.vicente@gigigo.com>
2018-01-10 08:59:08 +0100

    Initial Commit

---
+++ FunctionalHttpNetwork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunctionalHttpNetwork
+//
+//  Created by Carlos Vicente on 10/1/18.
+//  Copyright © 2018 cmvicentehe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
