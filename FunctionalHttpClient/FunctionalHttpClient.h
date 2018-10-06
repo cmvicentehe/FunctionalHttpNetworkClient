8161de9fe67afd15a979fc1f9dd9e315bb0b4e9f
Carlos M. Vicente Herrero <cmvicentehe@gmail.com>
2018-10-06 19:37:56 +0200

    - Project structure changed

---
+++ FunctionalHttpClient/FunctionalHttpClient.h
@@ -0,0 +1,19 @@
+//
+//  FunctionalHttpClient.h
+//  FunctionalHttpClient
+//
+//  Created by Carlos Manuel Vicente Herrero on 06/10/2018.
+//  Copyright © 2018 cmvicentehe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FunctionalHttpClient.
+FOUNDATION_EXPORT double FunctionalHttpClientVersionNumber;
+
+//! Project version string for FunctionalHttpClient.
+FOUNDATION_EXPORT const unsigned char FunctionalHttpClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunctionalHttpClient/PublicHeader.h>
+
+
